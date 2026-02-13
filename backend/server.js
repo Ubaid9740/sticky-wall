c3396a9e330f57118b237936b2f29e1b134c3a87
ubd <ubaidrehman9740@gmal.com>
2026-02-14 01:21:20 +0500

    Update API URL in frontend

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,7 @@ const cors = require('cors');
 const app = express();
 
 
-app.use(cors({
-    origin: "*", 
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 
 
 
